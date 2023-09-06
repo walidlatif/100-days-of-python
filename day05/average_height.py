d57c94166ce04118450d86830703e50dda68f9eb
walidlatif <walidlatif45@gmail.com>
2023-09-06 14:14:22 +0100

    Average height

---
+++ day05/average_height.py
@@ -0,0 +1,8 @@
+student_heights = input("Input a list of student heights ").split()
+sum = 0
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+    sum += student_heights[n]
+
+div = sum / len(student_heights)
+print(f"{round(div)}")
