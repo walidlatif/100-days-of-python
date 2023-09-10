b8eb4a74843e18f95175d4b9f16c301b67198ae6
walidlatif <walidlatif45@gmail.com>
2023-09-10 11:22:31 +0100

    Highest Score

--- day05/highest_score.py
+++ day05/highest_score.py
@@ -3,4 +3,5 @@ for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
     if int(student_heights[0]) < int(student_heights[n]):
         student_heights[0] = student_heights[n]
+print(student_heights)
 print("The highest score in the class is : {}".format(student_heights[0]))
