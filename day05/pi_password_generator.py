9ee8a5b102c62e76caa933279647127923b7a15e
walidlatif <walidlatif45@gmail.com>
2023-09-10 17:59:59 +0100

    Password Generator

---
+++ day05/pi_password_generator.py
@@ -0,0 +1,30 @@
+import random
+
+print("Welcome to the PyPassword Generator!")
+letters = int(input("How many letters would you like ?\n"))
+symbols = int(input("How many symbols would you like ?\n"))
+numbers = int(input("How many number would you like ?\n"))
+
+letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+symbol = "&*@$%#!?-+_=><|~/\\/{[]}():;,.\"'"
+number = "0123456789"
+_password = ""
+
+for i in range(letters):
+    le = random.choice(letter)
+    _password += le
+for j in range(symbols):
+    s = random.choice(symbol)
+    _password += s
+for k in range(numbers):
+    n = random.choice(number)
+    _password += n
+
+password_list = list(_password)
+# Shuffle the list in place
+random.shuffle(password_list)
+# Create a shuffled password string by joining the list of
+# characters ( Delete -> "[]" and "," )
+# from ['a', 'b', 'c', 'd'] to abcd
+shuffled_password = ''.join(password_list)
+print("Here is your password: {}".format(shuffled_password))
