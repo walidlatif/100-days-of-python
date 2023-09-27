084b521175c64e858382c1cd63313b5a4b95ace3
WALID LATIF <walidlatif45@gmail.com>
2023-09-27 23:35:01 +0100

    README

--- day05/average_height.py
+++ day05/average_height.py
@@ -3,6 +3,5 @@ sum = 0
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
     sum += student_heights[n]
-
 div = sum / len(student_heights)
 print(f"{round(div)}")
